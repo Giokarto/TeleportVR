c1ad9306971b9780f03a48e4091026ef5bbde059
missxa <aliona.kharchenko@gmail.com>
2021-08-25 11:56:03 +0200

    Limit menu rotation to only y axis

--- Assets/OperatorUserInterface/Scripts/HUD/MoveToCamera.cs
+++ Assets/OperatorUserInterface/Scripts/HUD/MoveToCamera.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 [ExecuteAlways]
 public class MoveToCamera : MonoBehaviour
 {
+
+    public float slerpSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,16 @@ public class MoveToCamera : MonoBehaviour
         try
         {
             transform.position = Camera.main.transform.position;
+            RotateYAxis(Camera.main.transform.rotation);
         }
         catch (System.NullReferenceException) { }
+    }
+
+    private void RotateYAxis(Quaternion target)
+    {
+        var yRot = new Vector3(0, target.eulerAngles.y, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, 
+            Quaternion.Euler(yRot), 
+            Mathf.Clamp01(Time.deltaTime * slerpSpeed));
     }
 }
