1df75cba19c6016155db4961c0bed08e62061f26
missxa <aliona.kharchenko@gmail.com>
2021-08-29 00:20:36 +0200

    added widget controls from animus client

--- Assets/AnimusClient/Example/Scripts/ClientLogic.cs
+++ Assets/AnimusClient/Example/Scripts/ClientLogic.cs
@@ -29,13 +29,14 @@ public class ClientLogic : Singleton<ClientLogic>
 
     IEnumerator ClientManagerLogic()
     {
+        
         // Yield for 10 frames to allow other scripts to initialise first
         if (_count < 10)
         {
             _count++;
             yield return null;
         }
-
+        
         // Step 1 - Login user
         print(AnimusManager);
         AnimusManager.LoginUser(AccountEmail, AccountPassword);
@@ -83,7 +84,8 @@ public class ClientLogic : Singleton<ClientLogic>
         if (_chosenRobot == null)
         {
             Debug.Log($"Robot {robotName} not found");
-            WidgetInteraction.SetAnimusStatus("WifiRed", $"Robot {robotName} not found");
+            
+            //WidgetInteraction.SetAnimusStatus("WifiRed", $"Robot {robotName} not found");
             yield break;
         }
 
@@ -99,6 +101,8 @@ public class ClientLogic : Singleton<ClientLogic>
             yield return null;
         }
         if (!AnimusManager.connectedToRobotSuccess) yield break;
+        
+
 
         // Step 5 - Starting all modalities
         AnimusManager.OpenModalities(requiredModalities);
