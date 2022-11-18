aade471e32a2f8132be163261c696de662c89c8c
Zuzana Urbanová <zu10urbanova@gmail.com>
2022-11-18 11:37:59 +0100

    add scripts to manage state / scenes together

--- Assets/Scripts/OperatorUserInterface/SceneManagement/SceneManager.cs
+++ Assets/Scripts/OperatorUserInterface/SceneManagement/SceneManager.cs
@@ -7,6 +7,12 @@ namespace OperatorUserInterface
         Empty // main scene that contains only necessary control elements for both scenes
     }
     
+    /// <summary>
+    /// TODO:
+    /// This class should combine <see cref="StateManager"/>, <see cref="OperatorUI.AdditiveSceneManager"/>,
+    /// <see cref="TransitionManager"/>, and <see cref="ConstructFXManager"/>.
+    /// One prefab object managing the state should be created.
+    /// </summary>
     public class SceneManager : Singleton<SceneManager>
     {
         public Scene currentScene;
