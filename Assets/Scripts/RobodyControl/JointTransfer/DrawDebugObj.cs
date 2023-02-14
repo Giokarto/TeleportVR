4847b90dab0a650700107b57467debafa84fe143
Zuzana Urbanová <zu10urbanova@gmail.com>
2023-02-14 12:52:11 +0100

    remove duplicate function

--- Assets/Scripts/RobodyControl/JointTransfer/DrawDebugObj.cs
+++ Assets/Scripts/RobodyControl/JointTransfer/DrawDebugObj.cs
@@ -13,29 +13,13 @@ namespace RobodyControl.JointTransfer
         // Start is called before the first frame update
         void Start()
         {
-            foreach (var child in getChildren(transform, g => g.name.Contains("_")))
+            foreach (var child in Utils.GetChildren(transform, g => g.name.Contains("_")))
             {
                 var cube = GameObject.CreatePrimitive(primitiveType);
                 cube.transform.parent = child.transform;
                 cube.transform.localPosition = Vector3.zero;
                 cube.transform.localRotation = Quaternion.identity;
                 cube.transform.localScale = new Vector3(scale, scale, scale);
-            }
-        }
-
-
-        private IEnumerable<GameObject> getChildren(Transform parent, System.Predicate<GameObject> predicate)
-        {
-            foreach (Transform c in parent)
-            {
-                if (predicate(c.gameObject))
-                {
-                    yield return c.gameObject;
-                    foreach (var tmp in getChildren(c, predicate))
-                    {
-                        yield return tmp;
-                    }
-                }
             }
         }
     }
